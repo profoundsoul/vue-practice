76dd3b49599e07467847a93cab0e65a50f28e529
lin.qiu <lin.qiu@yiwill.com>
2017-07-24 18:41:26 +0800

    add proxy

---
+++ vue-b2r/src/components/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>this is my login Page!!!</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'login',
+    data(){
+      return {
+        title: 'this is my login Page!!!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  h1{
+    color: red;
+    font-size:28px;
+  }
+</style>
--- vue-b2r/src/router/index.js
+++ vue-b2r/src/router/index.js
@@ -2,14 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
+    {name: 'Hello', path: '/', component: Hello},
+    {name: 'login', path: '/login', component:r=>require.ensure(['@/components/login.vue'], ()=>{r(require('@/components/login.vue'))}, 'login')}
   ]
 })
